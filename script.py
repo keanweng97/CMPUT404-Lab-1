e73bacce57160a6cc91587b820221e23cc525ece
Kean Weng Yap <yapkeanweng1223@gmail.com>
2021-01-11 18:52:52 -0700

    Add sections to code.

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import requests
 print(requests.__version__)
+
 print(requests.get("http://google.com"))
+
 response = requests.get("https://raw.githubusercontent.com/keanweng97/CMPUT404-Lab-1/main/script.py")
 print(response.text)
